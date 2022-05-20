2dc2d09e45d28fa0c431bc36cd0d3a42965a0596
Rafael Romero <rafa710@gmail.com>
2022-05-20 00:39:44 -0300

    agrega funcion para dibujar guiones de la palabra secreta

--- funciones.js
+++ funciones.js
@@ -1,3 +1,17 @@
+function dibujarGuionesPalabraSecreta(palabrasecreta){
+  const longitudDePalabraSecreta = palabrasecreta.length;
+  let guionesPalabraSecreta = ""; 
+
+  pincel.font = "30px Arial";
+  pincel.fillStyle = "white";
+  pincel.textAlign = "center";
+  for(let i = 0; i < palabrasecreta.length; i++){
+    guionesPalabraSecreta += "_ ";
+  }
+  pincel.fillText(guionesPalabraSecreta, tablero.width/2, 385);
+}
+
+
 const palabrasDelJuego = ["DEVOPS", "ALURA", "ORACLE", "JAVASCRIP", "HTML", "CSS", "JAVA", "PHYTON", "MYSQL", "CSHARP", "MONGODB", "RUBY", "TECLADO", "MOUSE", "MONITOR", "PARLANTE", "CPU", "GPU", "MEMORIA", "MOTHER", "RAM", "COOLER"];
 
 function crearPalabraSecreta(){
