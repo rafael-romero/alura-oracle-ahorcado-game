56214335b936c400b58f4f03592a0daa3a1c1398
Rafael Romero <rafa710@gmail.com>
2022-05-23 01:22:21 -0300

    agrega interaccion con sonido

--- funciones.js
+++ funciones.js
@@ -6,7 +6,6 @@ function mostrarElemento(elemento){
   elemento.classList.remove("oculto");
 }
 
-
 function inicializarVacioLetrasEquivocadas(){
   const MAXIMO_DE_LETRAS_EQUIVOCADAS = 24;
   for(let i = 0; i < MAXIMO_DE_LETRAS_EQUIVOCADAS; i++){
@@ -45,38 +44,49 @@ $btnIniciarJuego.onclick = function(event){
   mostrarElemento($contenedorBotonesNuevoJuegoDesistir);
 }
 
+const $sonidoSorpresa = document.querySelector("#sonido-sorpresa");
 function verificarQueNoSeaLetraRepetida(letraPresionada){
   if((letrasEquivocadas.includes(letraPresionada)) || (palabraFormadaPorElUsuario.includes(letraPresionada))){
     const mensaje = "Esta letra esta repetida, eliga otra por favor!!!";
     mostrarMensaje(mensaje);
+    $sonidoSorpresa.play();
     return false;
   }
   return true;
 }
 
+const $sonidoCorrecto = document.querySelector("#sonido-correcto");
+const $sonidoIncorrecto = document.querySelector("#sonido-incorrecto");
 function verificarSiLaLetraCoincide(letrapresionada){
   if(palabraSecreta.includes(`${letrapresionada}`)){
     dibujarLetraCorrecta(letrapresionada);  
+    $sonidoCorrecto.play();
   }else{
     dibujarLetraIncorrecta(letrapresionada);
     dibujarAhorcado();
+    $sonidoIncorrecto();
   }  
 }
 
+const $sonidoGanador = document.querySelector("#sonido-ganador");
+const $sonidoTeclaPresionada = document.querySelector("#sonido-presionar-tecla");
 const letrasAceptadasEnMayusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 document.addEventListener("keydown", function(event){
   const letraPresionada = event.key;
+  $sonidoTeclaPresionada.play();
   if(letrasAceptadasEnMayusculas.includes(`${letraPresionada}`)){
     if(verificarQueNoSeaLetraRepetida(letraPresionada)){
       verificarSiLaLetraCoincide(letraPresionada);
       if(JSON.stringify(palabraFormadaPorElUsuario) === JSON.stringify(palabraSecretaEnArray)){
         dibujarMensajeGanaste();
-        finalizarJuego();
+        $sonidoGanador.play();
+        dibujarFinDelJuego();
       }
     }
   }else{
     const mensaje = "Unicamente se aceptan letras mayusculas";
     mostrarMensaje(mensaje);
+    $sonidoSorpresa.play();
   }
 }, false);
 
@@ -94,7 +104,6 @@ function validarPalabra(palabra){
 }
 
 const $mensajesAlUsuario = document.querySelector("#mensajes-al-usuario");
-
 function eliminarError(){
   $mensajesAlUsuario.textContent = "";
   $mensajesAlUsuario.classList.remove("error");
@@ -124,6 +133,7 @@ $btnGuardarYEmpezar.onclick = function(event){
     eliminarError();
     palabrasDelJuego.push($inputPalabraSecretaAAgregar.value);
     const mensaje = "Su palabra a sido agregada!";
+    $sonidoCorrecto.play();
     mostrarMensaje(mensaje);
     ocultarElemento($contenedorBotonesAgregarPalabra);
   }
@@ -135,12 +145,12 @@ $btnCerrarModal.addEventListener("click",() => {
   $modal.close();
 });
  
-
 const $btnCancelarNuevaPalabra = document.querySelector("#cancelar-nueva-palabra");
 $btnCancelarNuevaPalabra.onclick = function(event){
   event.preventDefault();
   const mensaje = "No agrego ninguna palabra!";
   mostrarMensaje(mensaje);
+  $sonidoIncorrecto.play();
   ocultarElemento($contenedorBotonesAgregarPalabra);
 }
 
@@ -165,7 +175,6 @@ $btnNuevoJuego.onclick = function(event){
   reiniciarJuego();
   $btnIniciarJuego.click();
 }
-
 
 const $contenedorBotonesNuevoJuegoDesistir = document.querySelector("#contenedor-nuevo-juego-desistir");
 const $btnDesistir = document.querySelector("#desistir");
